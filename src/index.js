9d468fd7535f6b9a6aca476813b1615e4f09d512
Max <ivannikov.maxim@seznam.cz>
2021-09-08 19:17:32 +0200

    bindActionCreators3

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import {createStore, bindActionCreators} from 'redux';
 import reducer from './reducer';
-import { inc, dec, rnd, zero } from './actions';
+import * as actions from './actions';
 
 
 const store = createStore(reducer);
@@ -11,23 +11,15 @@ const {dispatch} = store;
 //   dispatch(creator(...args));
 // }
 
-const {incDispatch, decDispatch,rndDispatch, zeroDispatch} = bindActionCreators(
-  {
-    incDispatch: inc,
-    decDispatch: dec,
-    rndDispatch: rnd,
-    zeroDispatch: zero
-  }
-  ,dispatch
-);
+const {inc, dec,rnd, zero} = bindActionCreators({actions},dispatch);
 
 
-document.getElementById('inc').addEventListener('click', incDispatch);
-document.getElementById('dec').addEventListener('click', decDispatch);
-document.getElementById('zero').addEventListener('click', zeroDispatch);
+document.getElementById('inc').addEventListener('click', inc);
+document.getElementById('dec').addEventListener('click', dec);
+document.getElementById('zero').addEventListener('click', zero);
 document.getElementById('rnd').addEventListener('click', () => {
   const value = Math.floor(Math.random() * 10);
-  rndDispatch(value)
+  rnd(value)
 });
 
 const update = () => {
