fbc91d2ea599fe24f69461d083488fb7c5dcdacd
Max <ivannikov.maxim@seznam.cz>
2021-09-08 22:59:55 +0200

    provider connect

--- src/index.js
+++ src/index.js
@@ -1,33 +1,25 @@
+import React from 'react';
+import  ReactDOM  from 'react-dom';
 import {createStore, bindActionCreators} from 'redux';
+import { Provider } from 'react-redux';
 import reducer from './reducer';
-import * as actions from './actions';
-
+import App from './components/app';
 
 const store = createStore(reducer);
-
-const {dispatch} = store;
 
 // const bindActionCreator = (creator, dispatch) => (...args) => {
 //   dispatch(creator(...args));
 // }
 
-const {inc, dec,rnd, zero} = bindActionCreators({actions},dispatch);
+//const {inc, dec, rnd, zero} = bindActionCreators(actions, dispatch);
 
 
-document.getElementById('inc').addEventListener('click', inc);
-document.getElementById('dec').addEventListener('click', dec);
-document.getElementById('zero').addEventListener('click', zero);
-document.getElementById('rnd').addEventListener('click', () => {
-  const value = Math.floor(Math.random() * 10);
-  rnd(value)
-});
-
-const update = () => {
-  document.getElementById('counter').textContent = store.getState();
-}
+  ReactDOM.render(
+    <Provider store={store}>
+      <App/>
+    </Provider>, document.getElementById('root'));
 
 
-store.subscribe(update);
 
 // store.dispatch({type:'INC'});
 
